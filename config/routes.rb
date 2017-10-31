b5ab934bf97c3e3628a21187d918e148490a5326
Yas Kamitokusari <you@example.com>
2017-10-31 21:30:28 +0900

    add comment approve action

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     resources :blogs, only: %w(index new create edit show update) do
         resources :entries, only: %w(index new create edit show update) do
-            resources :comments
+            resources :comments do
+                member do
+                    post 'approve'
+                end
+            end
         end
     end
 end
